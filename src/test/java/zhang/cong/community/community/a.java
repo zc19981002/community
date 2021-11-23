fc32cc163df4f29283ce7e6e161a236b8eeaeb62
zc19981002 <1257927546@qq.com>
2021-11-23 23:06:45 +0800

    test111

--- src/test/java/zhang/cong/community/community/a.java
+++ src/test/java/zhang/cong/community/community/a.java
@@ -3,5 +3,6 @@ package test.java.zhang.cong.community.community;
 public class a {
     public static void main(String[] args) {
         System.out.println("11");
+        System.out.println("222");
     }
 }
